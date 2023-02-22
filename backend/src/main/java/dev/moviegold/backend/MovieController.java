b98004bb19fe2c178fc8af95cc6737c5b2f4cd84
Sheng Chou <chousheng@gmail.com>
2023-02-22 00:08:33 -0800

    Backend: Add MovieRepository and MovieService (01:03:30)

--- backend/src/main/java/dev/moviegold/backend/MovieController.java
+++ backend/src/main/java/dev/moviegold/backend/MovieController.java
@@ -1,5 +1,8 @@
 package dev.moviegold.backend;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/movies")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping
-    public ResponseEntity<String> allMovies() {
-        return new ResponseEntity<String>("All Movies!", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 }
