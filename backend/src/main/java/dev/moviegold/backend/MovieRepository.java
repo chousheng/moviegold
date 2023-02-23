f4568c74a6830e098333690f32ab7cf07078df66
Sheng Chou <chousheng@gmail.com>
2023-02-22 22:38:03 -0800

    Backend: Implement findMovieByImdbId (1:15:35)

--- backend/src/main/java/dev/moviegold/backend/MovieRepository.java
+++ backend/src/main/java/dev/moviegold/backend/MovieRepository.java
@@ -1,10 +1,10 @@
 package dev.moviegold.backend;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
-    
+    Optional<Movie> findMovieByImdbId(String imdbId); // Will be automatically generated
 }
