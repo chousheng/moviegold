f8af89d5aad962d62de86d055f0c9ebc19a5cc9c
Sheng Chou <chousheng@gmail.com>
2023-02-28 21:55:36 +0000

    Frontend: Add Reviews and ReviewForm components (02:39:40)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState();
 
   const getMovies = async () => {
     try {
@@ -19,6 +22,17 @@ function App() {
       console.log(err);
     }
   };
+
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviewIds);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   useEffect(() => {
     getMovies();
@@ -31,6 +45,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home movies={movies} />}></Route>
           <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
         </Route>
       </Routes>
     </div>
